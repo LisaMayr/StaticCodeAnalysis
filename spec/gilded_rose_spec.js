08daf307f1f81d2e2fb6f37297683f39f52f3d9b
Lisa Mayrhofer <if23b219@technikum-wien.at>
2024-06-02 23:48:02 +0200

    started to create a test for the quality decrease

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -19,15 +19,23 @@ describe("Gilded Rose", function() {
     }
   });
 
-  it("checks if quality degrades twice as fast after sell by date has been reached", function() {
+  it("checks if quality decreases twice as fast after sell_in reaches zero", function() {
+    // Arrange
+    var items = [];
+    items.push(new Item('+5 Dexterity Vest', 0, 20));
+    items.push(new Item('Elixir of the Mongoose', 0, 7));
+    items.push(new Item('Conjured Mana Cake', 0, 6));
+    //items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 0, 6));
+
+    // Act
     update_quality();
-    let qualityLimitIsMet = true;
-    for(var i = 0; i < items.length; i++) {
-      if(items[i].sell_in <= 0){
-        qualityLimitIsMet = false;
-      }
-      expect(qualityLimitIsMet).toEqual(true);
-    }
+
+    // Assert
+    expect(items[0].quality).toEqual(18); // +5 Dexterity Vest should decrease by 2
+    expect(items[1].quality).toEqual(5); // Elixir of the Mongoose should decrease by 2
+    expect(items[2].quality).toEqual(4); // Conjured Mana Cake should decrease by 4
+    //expect(items[3].quality).toEqual(0); // Conjured Mana Cake should decrease by 4
+    
   });
 
 });
